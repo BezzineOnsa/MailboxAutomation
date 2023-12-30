8d1a46623bf5617afa44df0fc7988f4ca910657b
OnsaBezzine <onsa.bezzine@esprit.tn>
2023-12-30 01:48:35 +0100

    main commit

---
+++ src/main/java/com/bezzine/mail/receiver/MailboxAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.bezzine.mail.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailboxAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailboxAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bezzine/mail/receiver/MailboxAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezzine.mail.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailboxAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
